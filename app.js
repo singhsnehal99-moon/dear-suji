77c76e9cdb60055bf20e36e96b2fbe5de34eaeaa
singhsnehal99-moon <singh.snehal99@gmail.com>
2026-08-24 13:51:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -39,16 +39,13 @@ dashboardBtn.onclick=()=>{
 
 /* ---------- Mood ---------- */
 
-document.querySelectorAll(".mood-btn").forEach(btn => {
-  btn.addEventListener("click", () => {
-
+document.querySelectorAll(".mood-btn").forEach(btn=>{
+  btn.onclick=()=>{
     document.querySelectorAll(".mood-btn")
-      .forEach(b => b.classList.remove("selected"));
-
-    btn.classList.add("selected");
-    mood = btn.dataset.mood;
-
-  });
+      .forEach(b=>b.classList.remove("active"));
+    btn.classList.add("active");
+    mood=btn.dataset.mood;
+  };
 });
 
 /* ---------- Load ---------- */
@@ -174,7 +171,7 @@ ${entry.proud||"-"}
 
 I love you endlessly 🤍`;
 
-  await fetch("https://discord.com/api/webhooks/1541403765377335368/CN4gZW2CzGd-Zdn-6eKrmL219FLwMY8VHuhDhkdgK7KZ-_W216KF7FDU9BDdbZLOnmbR",{
+  await fetch("YOUR_DISCORD_WEBHOOK",{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
