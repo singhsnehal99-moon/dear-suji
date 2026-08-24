c8260456f0a92a2935e553beacf786ba14fb5e73
singhsnehal99-moon <singh.snehal99@gmail.com>
2026-08-24 12:33:27 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDujzO37NdlU2H4z8GxC9TfmRoXUVjSsfM",
+  authDomain: "dear-suji.firebaseapp.com",
+  projectId: "dear-suji",
+  storageBucket: "dear-suji.firebasestorage.app",
+  messagingSenderId: "344382665682",
+  appId: "1:344382665682:web:2d25628dd9b90fe6ef8e0b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
